d61111d9b7406118e8cdc3a037946adf46369d49
doofensmith <wahyuwijanarko3998@gmail.com>
2021-07-22 16:59:44 +0700

    Commit wahyu

---
+++ app/src/main/java/com/softtech/hafy/Fragments/Beranda.java
@@ -0,0 +1,30 @@
+package com.softtech.hafy.Fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.softtech.hafy.R;
+
+
+public class Beranda extends Fragment {
+
+
+
+    public Beranda() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_beranda, container, false);
+
+        return rootView;
+    }
+}
--- app/src/main/java/com/softtech/hafy/MainActivity.java
+++ app/src/main/java/com/softtech/hafy/MainActivity.java
@@ -1,8 +1,12 @@
 package com.softtech.hafy;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+
+import com.softtech.hafy.Fragments.Beranda;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //inflate 1st fragment
+        changeFragments(new Beranda());
+
+    }
+
+
+    public boolean changeFragments (Fragment fragment) {
+
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.am_framelayout, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+
     }
 }
